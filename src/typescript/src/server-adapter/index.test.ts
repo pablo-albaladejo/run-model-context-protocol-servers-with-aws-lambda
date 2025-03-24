ba67698f0b490ddec26a626df5011ae185077579
Clare Liguori <liguori@amazon.com>
2025-03-24 10:34:24 -0700

    Initial set of tests for TS server adapter

--- src/typescript/src/server-adapter/index.test.ts
+++ src/typescript/src/server-adapter/index.test.ts
@@ -1,68 +1,155 @@
-import { JSONRPCMessage } from '@modelcontextprotocol/sdk/types.js';
 import {
-  StdioClientTransport,
-  StdioServerParameters,
-} from '@modelcontextprotocol/sdk/client/stdio.js';
+  JSONRPCNotification,
+  JSONRPCRequest,
+  JSONRPCResponse,
+} from '@modelcontextprotocol/sdk/types.js';
+import { StdioServerParameters } from '@modelcontextprotocol/sdk/client/stdio.js';
+import { Context } from 'aws-lambda';
+import { stdioServerAdapter } from './index.js';
 
 const serverParameters: StdioServerParameters = {
   command: 'npx',
   args: ['tsx', 'test-stdio-server/echo_server.ts'],
 };
 
-test('should start then close cleanly', async () => {
-  const client = new StdioClientTransport(serverParameters);
-  client.onerror = (error) => {
-    throw error;
+const mockContext: Context = {
+  callbackWaitsForEmptyEventLoop: true,
+  functionName: 'test-function',
+  functionVersion: '1',
+  invokedFunctionArn: 'test-arn',
+  memoryLimitInMB: '128',
+  awsRequestId: 'test-id',
+  logGroupName: 'test-group',
+  logStreamName: 'test-stream',
+  getRemainingTimeInMillis: () => 1000,
+  done: () => {},
+  fail: () => {},
+  succeed: () => {},
+};
+
+test('should respond to pings', async () => {
+  const pingMessage: JSONRPCRequest = {
+    jsonrpc: '2.0',
+    id: 1,
+    method: 'ping',
   };
 
-  let didClose = false;
-  client.onclose = () => {
-    didClose = true;
+  const expectedResponse: JSONRPCResponse = {
+    jsonrpc: '2.0',
+    id: 1,
+    result: {},
   };
 
-  await client.start();
-  expect(didClose).toBeFalsy();
-  await client.close();
-  expect(didClose).toBeTruthy();
+  const response = (await stdioServerAdapter(
+    serverParameters,
+    { ...pingMessage },
+    mockContext
+  )) as JSONRPCResponse;
+
+  expect(response).toEqual(expectedResponse);
 });
 
-test('should read messages', async () => {
-  const client = new StdioClientTransport(serverParameters);
-  client.onerror = (error) => {
-    throw error;
+test('should respond to initialize', async () => {
+  const initializeMessage: JSONRPCRequest = {
+    jsonrpc: '2.0',
+    id: 1,
+    method: 'initialize',
+    params: {
+      protocolVersion: '2024-11-05',
+      capabilities: {},
+      clientInfo: { name: 'mcp', version: '0.1.0' },
+    },
   };
 
-  const messages: JSONRPCMessage[] = [
-    {
-      jsonrpc: '2.0',
-      id: 1,
-      method: 'ping',
+  const response = (await stdioServerAdapter(
+    serverParameters,
+    { ...initializeMessage },
+    mockContext
+  )) as JSONRPCResponse;
+
+  expect(response.jsonrpc).toEqual('2.0');
+  expect(response.id).toEqual(1);
+  expect(response).toHaveProperty('result');
+});
+
+test('should list tools', async () => {
+  const listToolsMessage: JSONRPCRequest = {
+    jsonrpc: '2.0',
+    id: 1,
+    method: 'tools/list',
+  };
+
+  const expectedResponse: JSONRPCResponse = {
+    jsonrpc: '2.0',
+    id: 1,
+    result: {
+      tools: [
+        {
+          name: 'echo',
+          inputSchema: {
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            additionalProperties: false,
+            properties: {
+              message: { type: 'string' },
+            },
+            required: ['message'],
+            type: 'object',
+          },
+        },
+      ],
     },
-    {
-      jsonrpc: '2.0',
-      method: 'notifications/initialized',
+  };
+
+  const response = (await stdioServerAdapter(
+    serverParameters,
+    { ...listToolsMessage },
+    mockContext
+  )) as JSONRPCResponse;
+
+  expect(response).toEqual(expectedResponse);
+});
+
+test('should call a tool', async () => {
+  const callToolsMessage: JSONRPCRequest = {
+    jsonrpc: '2.0',
+    id: 1,
+    method: 'tools/call',
+    params: {
+      name: 'echo',
+      arguments: {
+        message: 'Hello world',
+      },
     },
-  ];
+  };
 
-  const readMessages: JSONRPCMessage[] = [];
-  const finished = new Promise<void>((resolve) => {
-    client.onmessage = (message) => {
-      console.log('Received message', message);
-      readMessages.push(message);
+  const expectedResponse: JSONRPCResponse = {
+    jsonrpc: '2.0',
+    id: 1,
+    result: {
+      content: [{ type: 'text', text: 'Echo: Hello world' }],
+    },
+  };
 
-      if (readMessages.length == 1) {
-        resolve();
-      }
-    };
-  });
+  const response = (await stdioServerAdapter(
+    serverParameters,
+    { ...callToolsMessage },
+    mockContext
+  )) as JSONRPCResponse;
 
-  await client.start();
-  console.log('Sending message 1');
-  await client.send(messages[0]);
-  console.log('Sending message 2');
-  await client.send(messages[1]);
-  console.log('Reading messages');
-  await finished;
+  expect(response).toEqual(expectedResponse);
+});
 
-  await client.close();
+test('accepts a notification', async () => {
+  const notification: JSONRPCNotification = {
+    jsonrpc: '2.0',
+    method: 'notifications/initialized',
+  };
+
+  const response = (await stdioServerAdapter(
+    serverParameters,
+    { ...notification },
+    mockContext
+  )) as JSONRPCResponse;
+
+  expect(response).toEqual({});
 });
